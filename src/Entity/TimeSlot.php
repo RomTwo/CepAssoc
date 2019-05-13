9fd4762b4d63c274630b7a6d5d724a4f3c1fd954
unknown <mdiabate638@gmail.com>
2019-05-13 13:58:16 +0200

    Ajout de l'entité TimeSlot

---
+++ src/Entity/TimeSlot.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TimeSlotRepository")
+ */
+class TimeSlot
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $day;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $startTime;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $endTime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?int
+    {
+        return $this->day;
+    }
+
+    public function setDay(int $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getStartTime(): ?\DateTimeInterface
+    {
+        return $this->startTime;
+    }
+
+    public function setStartTime(\DateTimeInterface $startTime): self
+    {
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->endTime;
+    }
+
+    public function setEndTime(\DateTimeInterface $endTime): self
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+}
