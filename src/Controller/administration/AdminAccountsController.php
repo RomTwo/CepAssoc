407003f5b343bbab216c049389e6821a279aacb3
romainpoitevin <poitevinromain@gmail.com>
2019-05-15 22:22:59 +0200

    delete account, begin update an account

--- src/Controller/administration/AdminAccountsController.php
+++ src/Controller/administration/AdminAccountsController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\administration;
 
 use App\Entity\Account;
+use App\Form\AccountAdminType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdminAccountsController extends AbstractController
 {
@@ -20,8 +22,44 @@ class AdminAccountsController extends AbstractController
         return $this->render('administration/accounts/accounts.html.twig', array('accounts' => $accounts));
     }
 
+    public function edit(Request $request, $id)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $account = $manager->getRepository(Account::class)->find($id);
+        if ($account !== null) {
+            $form = $this->createForm(AccountAdminType::class, $account);
+
+            if ($request->isMethod('POST') && $form->isValid()) {
+                $manager->flush();
+                $this->addFlash('success', "Le compte vient d'être modifié");
+                return $this->redirectToRoute('admin_accounts');
+            }
+        } else {
+            $this->addFlash('error', "Le compte n'existe pas");
+        }
+
+        return $this->render('administration/accounts/edit.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * Delete a user account by id
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function delete($id)
     {
+        $manager = $this->getDoctrine()->getManager();
+        $account = $manager->getRepository(Account::class)->find($id);
 
+        if ($account !== null) {
+            $manager->remove($account);
+            $manager->flush();
+            $this->addFlash('success', "Le compte vient d'être supprimé");
+        } else {
+            $this->addFlash('error', "Le compte n'existe pas");
+        }
+
+        return $this->redirectToRoute('admin_accounts');
     }
 }
---
+++ src/Form/AccountAdminType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountAdminType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('sex', ChoiceType::class, array(
+                    'choices' => array(
+                        'M' => 'M',
+                        'F' => 'F'
+                    ),
+                    'expanded' => false
+                )
+            )
+            ->add('birthDate', DateType::class, array(
+                    'widget' => 'choice',
+                    'years' => range(date('Y'), date('Y') - 100),
+                )
+            )
+            ->add('zipCode', TextType::class)
+            ->add('address', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('city', TextType::class)
+            ->add('roles')
+            ->add('valid', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
