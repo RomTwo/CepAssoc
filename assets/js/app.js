7a866ccc017ab0b996c76a86599e291f6579249e
Mahamadou Diabaté <mdiabate638@gmail.com>
2019-05-21 13:09:25 +0200

    Ajout de la relation ManyToMany entre TimeSlot et Adherent, Possibilité d'ajouter et de supprimer plusieurs créneaux lors de l'ajout ou de la suppression d'une activité, et nouvelles base visuelles des activités.

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,8 @@ $('#account_zipCode').focusout( function(){
         }
     })
 });
+
+$(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
--- assets/js/registration.js
+++ assets/js/registration.js
@@ -252,4 +252,13 @@ $('#adherent_zipCodeRep2').focusout( function(){
     })
 });
 
+$(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
 
