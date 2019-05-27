c2e7a449876f4d6bfc6cf25796283e91d3ffaa56
romainpoitevin <poitevinromain@gmail.com>
2019-05-27 13:51:50 +0200

    add picker event

--- assets/js/datetimepicker.js
+++ assets/js/datetimepicker.js
@@ -1,8 +1,3 @@
-/*$.datetimepicker.setLocale('fr');
-$('.datetimepicker').datetimepicker({
-    format: 'd-m-Y H:i'
-});*/
-
 $('.datetimepicker').bootstrapMaterialDatePicker({
     format: 'DD-MM-YYYY HH:mm',
     lang: 'fr'
